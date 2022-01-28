f7785df730a50b80af46fb55c0d2e80fba9a5e23
Kyle Grenier <kyle.grenier11@gmail.com>
2022-01-28 15:40:59 -0600

    Imported sample book data, updated template to display data

--- Storefront/playground/views.py
+++ Storefront/playground/views.py
@@ -1,7 +1,15 @@
+from csv import reader
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
 
 # Create your views here.
+ratings_data = pd.read_csv('./data/books.csv', on_bad_lines='skip')
+
 def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Kpatt'})
+    return render(request, 'hello.html', {'name': ratings_data})

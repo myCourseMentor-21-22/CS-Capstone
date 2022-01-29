0658d6f395c0c97beebcdb67c4fbf5bfdb5aa26a
Kyle Grenier <kyle.grenier11@gmail.com>
2022-01-29 01:19:41 -0600

    Created test book model

--- Storefront/playground/models.py
+++ Storefront/playground/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    isbn = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    year = models.IntegerField()
+    publisher = models.CharField(max_length=100)
+    def __str__(self):
+        return self.title
--- Storefront/playground/views.py
+++ Storefront/playground/views.py
@@ -1,15 +1,25 @@
-from csv import reader
 from django.shortcuts import render
 from django.http import HttpResponse
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import os
+import json
 
+from .models import Book
 
 # Create your views here.
 ratings_data = pd.read_csv('./data/books.csv', on_bad_lines='skip')
 
 def say_hello(request):
-    return render(request, 'hello.html', {'name': ratings_data})
+    qs = Book.objects.all()
+    readers = [{'ISBN': x.isbn, 'Book-Title': x.title, 'Year-Of-Publication': x.year, 'Publisher': x.publisher} for x in qs]
+
+    df = pd.DataFrame(readers)
+
+    json_records = df.reset_index().to_json(orient='records')
+    data = []
+    data = json.loads(json_records)
+    context = {'d', data}
+
+    return render(request, 'hello.html', context)

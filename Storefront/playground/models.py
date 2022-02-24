eb2f67492de310773bbc961628f003fc9853411a
Kyle Grenier <kyle.grenier11@gmail.com>
2022-02-24 12:44:01 -0600

    added personal files to github

--- Storefront/playground/models.py
+++ Storefront/playground/models.py
@@ -1,18 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class Book(models.Model):
-    isbn = models.CharField(max_length=100)
-    title = models.CharField(max_length=100)
-    year = models.IntegerField()
-    publisher = models.CharField(max_length=100)
-    def __str__(self):
-        return self.title
-
-class Student(models.Model):
-    stu_id = models.IntegerField()
-    name = models.CharField(max_length=100, null=True)
-
-    def __str__(self):
-        return str(self.stu_id) + ', ' + self.name
--- Storefront/playground/views.py
+++ Storefront/playground/views.py
@@ -1,16 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import json
-
-from .models import Book
 
 # Create your views here.
-
 def say_hello(request):
-    data = pd.read_csv('./data/cs210_capstone_project.csv')
-    context = {'d': data.to_html()}
-    return render(request, 'hello.html', context)
+    return render(request, 'hello.html', {'name': 'Kpatt'})

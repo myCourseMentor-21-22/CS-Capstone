b02c11d099f6d79ca5015744267e252f3bc2c24f
Kyle Grenier <kyle.grenier11@gmail.com>
2022-02-24 12:47:33 -0600

    fixing stuff

--- Storefront/playground/views.py
+++ Storefront/playground/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import json
+
+from .models import Book
 
 # Create your views here.
+
 def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Kpatt'})
+    data = pd.read_csv('./data/cs210_capstone_project.csv')
+    context = {'d': data.to_html()}
+    return render(request, 'hello.html', context)
